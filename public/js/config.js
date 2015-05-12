526de47cc6adea91f1a7e4b5e6be0d4d146b9491
Andrés <andpy.me@gmail.com>
2015-05-12 11:39:39 -0500

    create Stories module

--- public/js/config.js
+++ public/js/config.js
@@ -1,17 +1,12 @@
 'user strict';
 
 var AppConfiguration = (function() {
-    // Init module configuration options
     var app_module_name = 'club';
     var app_module_dependencies = ['ngResource', 'ui.router', 'ngSanitize',
         'ui.bootstrap'];
 
-    // Add a new vertical module
     var registerModule = function(module_name, dependencies) {
-        // Create angular module
         angular.module(module_name, dependencies || []);
-
-        // Add the module to the AngularJS configuration file
         angular.module(app_module_name).requires.push(module_name);
     };
 
