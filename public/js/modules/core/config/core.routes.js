2f61c30d33e4221651a03a4219a17f81982eff31
Andrés <andpy.me@gmail.com>
2015-05-12 12:03:48 -0500

    create core module; add 'global' navbar

---
+++ public/js/modules/core/config/core.routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('club').config(routes);
+
+routes.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function routes($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('app', {
+            url: '/',
+            templateUrl: 'public/templates/core/index.html'
+        })
+}
